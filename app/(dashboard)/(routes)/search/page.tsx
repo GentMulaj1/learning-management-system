318dd6e814bd4332eb0ca798b0f08a335e559176
GentMulaj1 <mulajgent2001@gmail.com>
2023-09-29 10:27:11 +0200

    Search Query

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,9 +1,25 @@
+import { db } from '@/lib/db'
 import React from 'react'
+import { Categories } from './_components/categories';
 
-const Search = () => {
+const Search = async () => {
+  const categories = await db.category.findMany({
+    orderBy: {
+      name: "asc",
+    }
+  });
+
+
+
+  
   return (
-    <div>
-      This is a Search PAGE
+    <div className='p-6'>
+      <Categories 
+        items={categories}
+      />
+
+
+
     </div>
   )
 }
